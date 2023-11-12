3e52a4b27c38ea78617e8a736ed45104f5bb3ee5
Sterlinghv <stervioo@gmail.com>
2023-11-12 11:36:26 -0700

    Messing around with parent -> child comm

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,15 +9,24 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   registerMode = false;
+  users: any; // bad
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-
+    this.getUsers();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode; // sets to opposite
   }
 
+  getUsers(){
+    // observable is a stream of data, want to observe what is coming back from out dotnet api
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has worked!')
+    })
+  }
 }
