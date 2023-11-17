b03cb48183ac8575c4c4452ee09d43274b048892
Sterlinghv <stervioo@gmail.com>
2023-11-16 22:29:26 -0700

    Middleware skeleton work in progress

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,9 @@
+﻿namespace API;
+
+public class ExceptionMiddleware
+{
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+    {
+        
+    }
+}
