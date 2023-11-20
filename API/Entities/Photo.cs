5ae1e5eee912be04b6bbf60e0efe3b802d24bb54
Sterlinghv <stervioo@gmail.com>
2023-11-19 17:02:16 -0700

    Database updates for user and photos

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities
 {
+    [Table("Photos")]
     public class Photo
     {
         public int Id { get; set; }
         public string Url { get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
+        public int AppUserId { get; set; } // this and..
+        public AppUser AppUser { get; set; } // this enforce that a photo must be tied to a user no "orphaned" photos
     }
 }
