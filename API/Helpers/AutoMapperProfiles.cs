63c8bfd3ccd1c6f9ac25367ef5bb0739caae0578
Sterlinghv <stervioo@gmail.com>
2023-11-21 21:49:34 -0700

    Automapper

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using AutoMapper;
+
+namespace API;
+
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        // auto mapper takes away tedious code from us
+        CreateMap<AppUser, MemberDto>();
+        CreateMap<Photo, PhotoDto>();
+    }
+}
