55b7a3e3ceca9abb4e651a490cdade54649a9238
Sterlinghv <stervioo@gmail.com>
2023-11-26 22:00:54 -0700

    Member list

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -11,23 +10,13 @@ export class HomeComponent {
   registerMode = false;
   users: any; // bad
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.getUsers();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode; // sets to opposite
-  }
-
-  getUsers(){
-    // observable is a stream of data, want to observe what is coming back from out dotnet api
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has worked!')
-    })
   }
 
   cancelRegisterMode(event: boolean) {
